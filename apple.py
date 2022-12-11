84dbc768720e0e54c9e7f7de444797e23efd3e83
Owen Chiaventone <ochiaventone@gmail.com>
2022-12-11 15:46:01 -0800

    Don't destroy terminal scrollback

--- apple.py
+++ apple.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 import time
 import sys
-
 HEIGHT=25
 WIDTH=60
-CLEAR="\033[2J"
+# This clear code actually moves the cursor to the top left and overwrites
+# in-place instead of adding each frame to scrollback history
+CLEAR="\033[H\033J"
 FRAMERATE=30
 INTERVAL=1.0/FRAMERATE
 
+# Push scrollback out of the way so we don't nuke the terminal history
+sys.stdout.write('\033[H\033[2J')
 with open('apple.txt', 'r') as f:
     while screen:=f.readlines(WIDTH*HEIGHT):
-        sys.stdout.write(CLEAR + "".join(screen))
+        # Omit the final newline to avoid blank line at the bottom
+        sys.stdout.write(CLEAR + "".join(screen)[:-2])
         sys.stdout.flush()
         time.sleep(INTERVAL)
